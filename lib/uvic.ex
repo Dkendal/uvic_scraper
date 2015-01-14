2a7df74354ee397db08fe6877c76a58744d9e178
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-01-13 19:44:45 -0800

    use named parameters instead of a dict

--- lib/uvic.ex
+++ lib/uvic.ex
@@ -1,48 +1,69 @@
+
+
+
 defmodule UVic do
   use HTTPoison.Base
 
-  defp default_params do
-    %{term: {"", "" },
-      course_range: {"", ""},
-      subject: "",
-      level: "" }
-  end
-
   def process_url(url) do
-    "https://www.uvic.ca/" <> url
+    "https://www.uvic.ca/BAN2P/" <> url
+  end
+  # course requirements
+  # /BAN2P/bwckctlg.p_disp_course_detail?
+  #   cat_term_in=201501
+  #   subj_code_in=CSC
+  #   crse_numb_in=320
+
+  # class schedules
+  # /BAN2P/bwckctlg.p_disp_listcrse?
+  #   term_in=201501
+  #   subj_in=CSC
+  #   crse_in=320
+  #   schd_in=
+
+  # class size information
+  # /BAN2P/bwckschd.p_disp_detail_sched?
+  #   term_in=201501
+  #   crn_in=20711
+
+  # course listings
+  # /BAN2P/bwckctlg.p_display_courses?
+  #   term_in=201501
+  #   one_subj=CSC
+  #   sel_crse_strt=320
+  #   sel_crse_end=320
+  #   sel_subj=
+  #   sel_levl=
+  #   sel_schd=
+  #   sel_coll=
+  #   sel_divs=
+  #   sel_dept=
+  #   sel_attr=
+
+
+  def display_courses(year, month, course_start \\ "", course_end \\ "", subjects \\ "") do
+    q = [
+      { :term_in, year <> month },
+      { :sel_crse_strt, course_start },
+      { :sel_crse_end, course_end },
+      { :sel_subj, "" },
+      { :sel_levl, "" },
+      { :sel_schd, "" },
+      { :sel_coll, "" },
+      { :sel_divs, "" },
+      { :sel_dept, "" },
+      { :sel_attr, "" } ]
+
+    q ++ if (is_list subjects) do
+      Enum.map subjects, fn (x) -> {:sel_subj, x} end
+    else
+      [ {:one_subj, subjects} ]
+    end
+
+    get!("bwckctlg.p_display_courses?" <> URI.encode_query(q))
   end
 
-  def display_courses do
-    display_courses(%{})
-  end
-
-  def display_courses(options) do
-    get! ("BAN2P/bwckctlg.p_display_courses?" <> build_query_string( options ))
-  end
-
-  defp build_query_string(map) do
-    %{term: {year, month },
-      course_range: {course_start_range, course_end_range},
-      subject: subject,
-      level: level } = map
-
-    URI.encode_query %{ term_in: year <> month,
-      one_subj: "",
-      sel_crse_strt: course_start_range,
-      sel_crse_end: course_end_range,
-      sel_subj: subject,
-      sel_levl: level,
-      sel_schd: "",
-      sel_coll: "",
-      sel_divs: "",
-      sel_dept: "",
-      sel_attr: "",
-    }
-  end
-
-  def subject_list(term) do
-    ( Map.merge(default_params, %{ term: term })
-    |> display_courses ).body
+  def subject_list(year, month) do
+    display_courses(year, month).body
     |> Floki.find("#subj_id")
     |> Floki.find("option")
     |> Floki.attribute("value")
