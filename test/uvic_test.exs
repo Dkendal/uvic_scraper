ed590d0c44afb53f31dd7094cdf122e2fd0f6fb5
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-01-17 16:26:38 -0800

    use vcr in test

--- test/uvic_test.exs
+++ test/uvic_test.exs
@@ -1,15 +1,24 @@
 defmodule UVicTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+
+  setup_all do
+    UVic.start
+  end
 
   test "subject_list" do
-    result = UVic.subject_list("2015", "01")
-    assert is_list result
-    assert "CYCB" = hd result
+    use_cassette "display_courses_term_only" do
+      result = UVic.subject_list("2015", "01")
+      assert is_list result
+      assert "CYCB" = hd result
+    end
   end
 
   test "display_courses" do
-    assert_raise FunctionClauseError, fn ->
-      UVic.display_courses("2015", "01", "")
+    use_cassette "display_courses_term_only" do
+      assert_raise FunctionClauseError, fn ->
+        UVic.display_courses("2015", "01", "")
+      end
     end
   end
 end
