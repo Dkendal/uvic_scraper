0c243c5741121973b0386755510986f90fe585ff
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-03-03 23:28:19 -0800

    simplify ast output

--- lib/abstract_syntax_tree.ex
+++ lib/abstract_syntax_tree.ex
@@ -18,15 +18,7 @@ defmodule AbstractSyntaxTree do
     bottom_up_parse Enum.reverse(r), []
   end
 
-  def bottom_up_parse [ {:course, _} = h | t ], r do
-    bottom_up_parse t, [ { :expr, [ h ] } | r ]
-  end
-
-  def bottom_up_parse [ { :expr, _ } ] = h, [] do
-    bottom_up_parse [], [ { :start, h } ]
-  end
-
-  def bottom_up_parse [ { :start, _ } ] = r, [] do
+  def bottom_up_parse [ { :expr, _ } ] = r, [] do
     r
   end
 
@@ -38,9 +30,23 @@ defmodule AbstractSyntaxTree do
     bottom_up_parse t, [ h | r ]
   end
 
-  def bottom_up_parse [ { :expr, _ }, { :op, _ }, { :expr, _ }| t ] = l, r do
+  defp bup l, t, r do
     bottom_up_parse t, [ { :expr, l -- t } | r ]
   end
+
+  def bottom_up_parse [ { :expr, _ }, { :op, _ }, { :expr, _ }| t ] = l, r do
+    bup l, t, r
+  end
+  def bottom_up_parse [ { :course, _ }, { :op, _ }, { :expr, _ }| t ] = l, r do
+    bup l, t, r
+  end
+  def bottom_up_parse [ { :course, _ }, { :op, _ }, { :course, _ }| t ] = l, r do
+    bup l, t, r
+  end
+  def bottom_up_parse [ { :expr, _ }, { :op, _ }, { :course, _ }| t ] = l, r do
+    bup l, t, r
+  end
+
     #[ lhs: _, expr: _, rhs: _ ] -> :expr
     #[ expr: _, op: _, expr: _ ] -> :expr
   def bottom_up_parse([h | t], r) when is_binary h do
--- test/abstract_syntax_tree_test.exs
+++ test/abstract_syntax_tree_test.exs
@@ -2,33 +2,20 @@ defmodule ASTTest do
   use ExSpec
   alias AbstractSyntaxTree, as: AST
 
-  @tag focus: true
   describe "bottom_up_parse" do
     context "the string is in the grammar" do
       it "returns a parse tree" do
         input = String.split "( AAAA 100 D or BBBB 200 D ) and CCCC 300 A+"
         assert AST.bottom_up_parse(input, []) == [
-          start: [
+          expr: [
             expr: [
-              expr: [
-                expr: [
-                  course: [
-                    subject: "AAAA",
-                    number: "100",
-                    grade: "D" ] ],
-                op: "or",
-                expr: [
-                  course: [
-                    subject: "BBBB",
-                    number: "200",
-                    grade: "D" ] ] ],
-              op: "and",
-              expr: [
-                course: [
-                  subject: "CCCC",
-                  number: "300",
-                  grade: "A+" ] ] ] ] ]
+              course: [ subject: "AAAA", number: "100", grade: "D" ],
+              op: "or",
+              course: [ subject: "BBBB", number: "200", grade: "D" ] ],
+            op: "and",
+            course: [ subject: "CCCC", number: "300", grade: "A+" ] ] ]
       end
+
     end
   end
 end
